50740763531016eb4e3d3634af6299cf23316f80
solution2024 <verifymail00010@gmail.com>
2024-08-20 20:08:07 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+// counter up code
+$('.counter_up').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+// slick slider
+$('.teamSlide').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000000,
+    arrows:false
+  });
